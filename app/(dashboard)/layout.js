514e1a0b3cabf50e30c21bd7f0bd70f6f39e3e71
aditya-yadav08 <aditya.yadav2708@gmail.com>
2024-02-29 21:55:46 +0530

    First Commit

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import SideNav from './_components/SideNav'
+
+function layout({children}) {
+  return (
+    <div>
+      <div className='  h-full w-64 flex-col fixed inset-y-0 z-50'>
+        <SideNav/>
+      </div>
+      <div className='ml-64'>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default layout
