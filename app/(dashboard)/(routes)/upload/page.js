9776008b9b4ca634e09caff9317397770d1ba56c
aditya-yadav08 <aditya.yadav2708@gmail.com>
2024-03-06 12:54:49 +0530

    Second Commit

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,8 +1,18 @@
+'use client';
+
 import React from 'react'
+import UploadForm from './_components/UploadForm'
 
 function Upload() {
   return (
-    <div>Upload</div>
+    <div className='p-5 px-8 md:px-28'>
+      <h2 className='text-[20px] text-center m-5'>
+        Start
+        <strong className='text-primary'> Uploading </strong>
+        File and <strong className='text-primary'> Share it</strong>
+      </h2>
+      <UploadForm/>
+    </div>
   )
 }
 
